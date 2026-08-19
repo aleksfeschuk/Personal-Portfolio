dba82b85e477bca220ec67f4dbaa61e9b045e9b7
aleksfeschuk <aleksfeschuk@gmail.com>
2026-08-19 06:54:39 -0600

    working with sidebar side

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -18,6 +18,10 @@ import {MenuIcon} from "lucide-react"
 
 import { navLinks, socialLinks } from "@/constants"
 
+// Custom modules
+
+import { cn } from "@/lib/utils"
+
 
 export const Sidebar = () => {
 
@@ -27,10 +31,49 @@ export const Sidebar = () => {
         <>
             <Sheet>
                 <SheetTrigger asChild>
-                    <Button>
-                        <MenuIcon/>
+                    <Button 
+                        variant='ghost'
+                        size='icon'
+                        className='m-4 fixed top-4 right-4 z-50 border-2 hover:border-primary bg-neutral-800 py-5 px-5 rounded-full hover:text-primary cursor-pointer'>
+                        <MenuIcon size={30}/>
                     </Button>
                 </SheetTrigger>
+                <SheetContent
+                    side='right'
+                    className='w-84 bg-neutral-900 text-white py-6 pl-10 '
+                >
+                    <SheetTitle className='text-lg font-semibold'>Menu</SheetTitle>
+                    <nav className="flex flex-col gap-4">
+                        {navLinks.map((link) => {
+                            const Icon = link.icon;
+
+                            return <a 
+                                key={link.label}
+                                href={link.link}
+                                onClick={() => setActive(link.link)}
+                                className={cn('text-neutral-300 flex items-center gap-2 hover:text-primary transition-colors duration-200 text-base',
+                                    active === link.link && 'text-primary'
+                                )}
+                                >
+                                <Icon className='size-4'/>{link.label}
+                            </a>
+                        })}
+                    </nav>
+                    <div className='mt-30'>
+                        <p className="pb-2">Socials</p>
+                        <div className="flex gap-3 text-neutral-500">
+                            {socialLinks.map((social, i) => {
+                                const Icon = social.icon
+
+                                return (
+                                    <a key={i} href={social.link}>
+                                        <Icon className='size-4'/>
+                                    </a>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </SheetContent>
             </Sheet>
         </>
     )
